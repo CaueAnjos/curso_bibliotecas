f7541c4c09c9d6b332d71f4aea1f33b0b9def052
CaueAnjos <kawidosanjos@outlook.com>
2025-05-31 15:59:57 -0300

    refectoring FuncionarioAutenticavel.cs

--- bytebanck_Modelos/bytebank.Modelos/ADM/Funcionarios/FuncionarioAutenticavel.cs
+++ bytebanck_Modelos/bytebank.Modelos/ADM/Funcionarios/FuncionarioAutenticavel.cs
@@ -5,16 +5,18 @@ namespace bytebank.Modelos.ADM.Funcionarios
 {
     public abstract class FuncionarioAutenticavel : Funcionario, IAutenticavel
     {
-        public string Senha { get; set; }
+        public string? Senha { get; set; }
 
         public FuncionarioAutenticavel(double salario, string cpf)
             : base(salario, cpf)
         {
-
         }
 
         public bool Autenticar(string senha)
         {
+            if (string.IsNullOrEmpty(Senha))
+                throw new NullReferenceException("Senha is null");
+
             return Senha == senha;
         }
     }
